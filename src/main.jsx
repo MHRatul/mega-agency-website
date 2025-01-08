2741d0b33bee7f6118e32f9c5e427ff3e2968594
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-01-09 01:14:16 +0600

    Essential folder create under-src

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
       <Routes>
         <Route element={<App/>}>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/pricing" element={<div>Pricing Page</div>} />
           <Route path="/services" element={<div>services Page</div>} />
           <Route path="/blogs" element={<div>Blogs Page</div>} />
