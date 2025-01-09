efce3ff33c126c06fe28fcceda0920b95e9ad343
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-01-09 11:32:51 +0600

    Make Hero Section

---
+++ src/pages/home/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='bg-colorBg'>
+        <div className='max-w-screen-2xl container mx-auto py-20 px-5'>
+            {/* left side */}
+            <div>Left side</div>
+
+            {/* right side */}
+            <div>Right side</div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
