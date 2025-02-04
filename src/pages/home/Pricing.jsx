e30e64b508dfbf972237072d9cf42f9ad66408e1
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-02-04 21:49:40 +0600

    Toggle button is used-in  pricing section

--- src/pages/home/Pricing.jsx
+++ src/pages/home/Pricing.jsx
@@ -9,11 +9,16 @@ const Pricing = () => {
         plan that works for you"/>
 
         {/* {Toggle button} */}
-        <div>
+        <div  className='text-center mb-10'>
             <label className='inline-flex  items-center cursor-pointer'>
                 <span className='mr-3 text-lg text-gray-600'>
                     {isYearly ? 'Yearly Pricing' : 'Monthly Pricing'}
                 </span>
+                <div className='relative'>
+                    <input type='checkbox' checked={isYearly} onChange={() => setIsYearly(!isYearly)} className='sr-only'/>
+                    <div className='toggle-bg w-14 h-8 rounded-full bg-gray-300'></div>
+                    <div className={`toggle-circle absolute top-1 left-1 w-6 h-6 bg-blue-600 rounded-full transition-transform duration-300     ${isYearly ? 'transform translate-x-6' : 'translate-x-0'}`}></div>
+                </div>
             </label>
         </div>
     </div>
