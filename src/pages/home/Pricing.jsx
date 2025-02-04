f237161743d2590db0b07d1bd3c0d8f094b69cbf
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-02-04 21:30:34 +0600

    useState is used- for pricing section

--- src/pages/home/Pricing.jsx
+++ src/pages/home/Pricing.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HeadingSection from '../../components/HeadingSection'
 
 const Pricing = () => {
+    const [isYearly, setIsYearly] = useState(false)
   return (
-    <div>Pricing</div>
+    <div className='section-container'>
+        <HeadingSection heading="Choose Your Plan" subheading="Pricing" description=" Our Pricing plans are designed to meet your needs and budget.Choose a
+        plan that works for you"/>
+
+        {/* {Toggle button} */}
+        <div>
+            <label className='inline-flex  items-center cursor-pointer'>
+                <span className='mr-3 text-lg text-gray-600'>
+                    {isYearly ? 'Yearly Pricing' : 'Monthly Pricing'}
+                </span>
+            </label>
+        </div>
+    </div>
   )
 }
 
