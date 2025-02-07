960901cd60a9e888739168130501447d630d39a5
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-02-08 01:22:34 +0600

    Logout button fully - worked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { Link } from "react-router";
-
+import React from 'react'
+import { Link } from 'react-router'
+import { useAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+
+    const { user, logout } = useAuth();
+    console.log(user)
+
+    const handleLogOut = () => {
+        logout();
+    }
+
     return (
-        <div className="navbar max-w-screen-2xl container mx-auto bg-white px-5">
+        <div className="navbar max-w-screen-2xl container mx-auto bg-white px-5 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -13,42 +21,40 @@ const Navbar = () => {
                             className="h-5 w-5"
                             fill="none"
                             viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
+                            stroke="currentColor">
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16"
-                            />
+                                d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content font-bold bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/blogs">Blogs</Link></li>
-                    <li>
-                        <details>
-                            <summary>Categories</summary>
-                            <ul className="p-2 z-50">
-                                <li><Link>Web Design</Link></li>
-                                <li><Link>App Development</Link></li>
-                                <li><Link>UI/UX Design</Link></li>
-                                <li><Link>Digital Marketing</Link></li>
-                            </ul>
-                        </details>
-                    </li>
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/services">Services</Link></li>
+                        <li><Link to="/blogs">Blogs</Link></li>
+                        <li>
+                            <details>
+                                <summary>Categories</summary>
+                                <ul className="p-2">
+                                    <li><Link to="/" >Web Design</Link></li>
+                                    <li><Link to="/" >App Development</Link></li>
+                                    <li><Link to="/" >UI/UX Design</Link></li>
+                                    <li><Link to="/" >Digital Marketing</Link></li>
+                                </ul>
+                            </details>
+                        </li>
                     </ul>
                 </div>
-                <Link to="/" className="text-xl font-bold flex item-center">
+                <Link to="/" className="text-xl font-bold flex items-center">
                     <img src="/logo.png" alt="" />
-                    <span className="ml-2 text-[#697077] hidden sm:block">MegaCode</span>
+                    <span className='ml-2 text-[#697077] hidden sm:block'>WebCode</span>
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 font-semibold from-neutral-400">
+                <ul className="menu menu-horizontal px-1">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/blogs">Blogs</Link></li>
@@ -57,21 +63,24 @@ const Navbar = () => {
                             <summary>Categories</summary>
                             <ul className="p-2 z-50">
                                 <li><Link to="/" >Web Design</Link></li>
-                                <li><Link to="/">App Development</Link></li>
-                                <li><Link to="/">UI/UX Design</Link></li>
-                                <li><Link to="/">Digital Marketing</Link></li>
+                                <li><Link to="/" >App Development</Link></li>
+                                <li><Link to="/" >UI/UX Design</Link></li>
+                                <li><Link to="/" >Digital Marketing</Link></li>
                             </ul>
                         </details>
                     </li>
                 </ul>
             </div>
             <div className="navbar-end space-x-4">
-                <Link to="/login" className="bg-transparent border border-blue-600 text-blue-600 py-3 px-4 rounded-md
-                          hover:bg-blue-600 hover:text-white transition">Log In</Link>
-                <Link to="/pricing" className="bg-blue-600 text-white py-3 px-4 rounded-md flex justify-center items-center hover:bg-blue-700 transition">Start Free Trial</Link>
+                {
+                    user ? (<><Link to="/dashboard" className="btn btn-outline  hover:bg-blue-800 px-8 hidden sm:flex">Dashboard</Link>
+                        <button onClick={handleLogOut}  className="btn  bg-blue-600 hover:bg-blue-800 text-white">Logout</button></>) : (<><Link to="/login" className="btn btn-outline btn-primary px-8 hidden sm:flex">Log In</Link>
+                            <Link to="/pricing" className="btn  bg-blue-600 hover:bg-blue-800  text-white">Start Free Trial</Link></>)
+                }
+
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
